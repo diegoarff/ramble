901181821b422f0bd6574593e97c1f1783ed63d5
agetro2003 <jesusdaniolob@gmail.com>
2023-11-09 23:20:37 -0400

    Feat: add liked function on tweets

--- src/app/components/tweet/tweet.component.ts
+++ src/app/components/tweet/tweet.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { ITweet } from 'src/app/interfaces/Tweets';
+import { TweetsService } from 'src/app/services/tweets.service';
 
 @Component({
   selector: 'app-tweet',
@@ -8,9 +9,10 @@ import { ITweet } from 'src/app/interfaces/Tweets';
   styleUrls: ['./tweet.component.scss'],
 })
 export class TweetComponent  implements OnInit {
-router = inject(Router);
+private router = inject(Router)
+private tweetService = inject(TweetsService)
   @Input() tweet: ITweet = {} as ITweet;
-
+loading: boolean = false;
   ngOnInit() {
     
   }
@@ -20,6 +22,22 @@ router = inject(Router);
   redirectToUser() {
     this.router.navigate(['/view-user', this.tweet.user[0]._id]);
     }
-
+  async likeTweet () {
+    if (this.loading) return;
+    this.loading = true;
+      const res = await this.tweetService.likeTweet(this.tweet._id!)
+      
+      if (res.status === 'success') {
+        if (res.message === 'Tweet liked') {
+          this.tweet.likeCount ++;
+          this.tweet.liked = true;
+        } else if (res.message === 'Tweet unliked') {
+          this.tweet.likeCount --;
+          this.tweet.liked = false;
+        }  
+      }
+      this.loading = false;
+      
+    }
 
 }
